7bf39652f7144a6a7860c00bad09141b45cacf5e
codigo <ogonzalez@perceptivatech.com>
2017-06-01 19:36:35 -0300

    actualization al 1/6

--- wingchun/src/pages/home/home.ts
+++ wingchun/src/pages/home/home.ts
@@ -1,14 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Slides } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  private slides2 = [];
+  private showStart = true;
+  private showVingTsung = false;
+  private showLinage = false;
+  private showContacto = false;
+  private showVT = false;
+  private showCF = false;
+  private showL = false;
+  private showHistoria = false;
 
+  @ViewChild(Slides) slides: Slides;
   constructor(public navCtrl: NavController) {
+    this.slides2 = [
+      {
+        //title: "Welcome to the Docs!",
+        //description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+        image: "assets/img/vt1.png",
+      },
+      {
+        //title: "Welcome to the Docs!",
+        //description: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+        image: "assets/img/vt1.png",
+      }
+    ];
 
   }
-
+  hideAll(){
+    this.showStart = false;
+    this.showVingTsung = false;
+    this.showLinage = false;
+    this.showContacto = false;
+  }
+  goHome(){
+    this.hideAll();
+    this.showStart = true;
+  }
+  goVT(){
+    this.hideAll();
+    this.showVingTsung = true;
+  }
+  goLinage(){
+    this.hideAll();
+    this.showLinage = true;
+  }
+  goContacto(){
+    this.hideAll();
+    this.showContacto = true;
+  }
+  hideVT(){
+    this.showVT = false;
+    this.showCF = false;
+    this.showL = false;
+    this.showHistoria = false;
+  }
+  goVTDetails(){
+    if(this.showVT){
+      this.hideVT();
+    }else{
+      this.showVT = !this.showVT;
+    }
+  }
+  goCF(){
+    if(this.showCF){
+      this.hideVT();
+    }else{
+      this.showCF = !this.showCF;
+    }
+  }
+  
 }
